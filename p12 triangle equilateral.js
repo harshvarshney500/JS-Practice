f059eae45ac304edb48496a734184477c5a7fe99
Harsh Varshney <126553654+harshvarshney500@users.noreply.github.com>
2023-07-01 09:36:05 +0530

    Create p12 triangle equilateral.js

---
+++ p12 triangle equilateral.js
@@ -0,0 +1,6 @@
+/*2 . Create a function Named Triangle which takes 3 parameters, say A, B and C denoting the 3 sides of a triangle.
+Use Callback function, create a method to check if the triangle is Equilateral or not.
+
+
+Output:
+"true" if the triangle is equilateral and "false" if its not.*/
