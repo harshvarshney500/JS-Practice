740efcb489ce4537d0a81de95576dafe01aab879
Harsh Varshney <126553654+harshvarshney500@users.noreply.github.com>
2023-07-02 11:20:15 +0530

    Update p24  intersection of two array.js

--- p24  intersection of two array.js
+++ p24  intersection of two array.js
@@ -3,3 +3,17 @@
 ex:- arr1 = [2,4,1,7,4,2] , arr2 = [5,1,4,7,3,1];
 
 output = [1,4,7]*/
+
+
+
+function intersection(){
+    var arr1=[2,4,1,7,4,2]; 
+    var arr2 = [5,1,4,7,3,1];
+    var l=[];
+    arr1.reduce((acc,e)=>{
+        if(arr2.includes(e)){
+            l.push(e);
+        }
+    });
+    return l;
+}
