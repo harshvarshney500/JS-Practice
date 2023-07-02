1be71fa2e058484d5e01980d23e01acc158e2564
Harsh Varshney <126553654+harshvarshney500@users.noreply.github.com>
2023-07-02 11:17:14 +0530

    Update p23  rotate an array bu no. of steps to right.js

--- p23  rotate an array bu no. of steps to right.js
+++ p23  rotate an array bu no. of steps to right.js
@@ -4,3 +4,11 @@ ex:- arr = [1,2,3,4,5];
 		step = 2;
 
  output = [5,4,1,2,3]*/
+
+
+function rotatestep(arr, step) {
+    for(var i = 0 ; i < step ; i++){
+        arr.unshift(arr.pop());
+    }
+    return arr;
+};
