939ecbdac4af28461f140ebb37c141722d6d7f6e
Harsh Varshney <126553654+harshvarshney500@users.noreply.github.com>
2023-07-02 11:01:05 +0530

    Update p17 reverse of string usin reverse method().js

--- p17 reverse of string usin reverse method().js
+++ p17 reverse of string usin reverse method().js
@@ -1,3 +1,15 @@
 /* Reverse a given string using reverse() method 
 input = "BrainMentors"
 output = "srotnemniarB"*/
+
+
+
+
+
+
+in_arr = input.split("");
+(12) ['B', 'r', 'a', 'i', 'n', 'M', 'e', 'n', 't', 'o', 'r', 's']
+in_arr.reverse();
+(12) ['s', 'r', 'o', 't', 'n', 'e', 'M', 'n', 'i', 'a', 'r', 'B']
+in_arr.join('');
+'srotneMniarB'
