7f732a6ed4e54b5a2274583ce2e7d8767e540e29
Harsh Varshney <126553654+harshvarshney500@users.noreply.github.com>
2023-07-02 11:10:53 +0530

    Update p20 reverse the order of ele. in arr.js

--- p20 reverse the order of ele. in arr.js
+++ p20 reverse the order of ele. in arr.js
@@ -3,3 +3,23 @@ don't Use reverse() Method
 ex:- var arr = ['a','b','c','d','e','f']
 			
 		output = ['f','e','d','c','b','a']*/
+
+
+
+
+var arr = ['a','b','c','d','e','f'];
+
+var rev_arr = [];
+
+for (var i = arr.length; i>=0; i--){
+    rev_arr.push(arr[i]);
+}
+
+
+
+
+7
+
+
+rev_arr;
+(7) [undefined, 'f', 'e', 'd', 'c', 'b', 'a']
