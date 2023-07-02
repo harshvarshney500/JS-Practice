078eec1e0ab66a0ad6d1c14ecfa055b96ceb431b
Harsh Varshney <126553654+harshvarshney500@users.noreply.github.com>
2023-07-02 11:06:28 +0530

    Update p18 counting no. of vowel in string.js

--- p18 counting no. of vowel in string.js
+++ p18 counting no. of vowel in string.js
@@ -2,3 +2,19 @@
 Count of vowels in string;
 ex:- str = "brainmentors"
 		output: count = 4*/
+
+
+function match_str(str){
+    const count = str.match(/[aeiou]/g).length;
+    return count;
+}
+undefined
+function in_str(){
+    var string = prompt("Enter the string");
+    len = match_str(string);
+    console.log(len);
+}
+undefined
+in_str();
+VM3115:4 4
+undefined
