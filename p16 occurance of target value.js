b086ee652c5652a9e9a1d9099edf469a0c618c2b
Harsh Varshney <126553654+harshvarshney500@users.noreply.github.com>
2023-07-02 11:00:18 +0530

    Update p16 occurance of target value.js

--- p16 occurance of target value.js
+++ p16 occurance of target value.js
@@ -1,3 +1,10 @@
 /*Find Occurance of a target value in given array;
 arr= [19, 22, 18, 19, 16, 18, 19, 21, 24];
 target = 19;*/
+
+
+
+var arr=[19, 22, 18, 19, 16, 18, 19, 21, 24]
+arr.filter(ele=>ele==19).length;
+VM1005:9 3
+undefined
